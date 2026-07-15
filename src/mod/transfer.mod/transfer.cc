eb69da824f6087e04151eabcccd8281140ed7478
Jeremiah Soliz <ominous@gmail.com>
2026-07-15 17:59:00 -0400

    Stop DCC sends on short file reads

--- src/mod/transfer.mod/transfer.cc
+++ src/mod/transfer.mod/transfer.cc
@@ -98,10 +98,14 @@ static unsigned long pump_file_to_sock(FILE *file, long sock,
   if (bf) {
     do {
       actual_size = pending_data >= buf_len ? buf_len : pending_data;
-      if (fread(bf, actual_size, 1, file)) {
-        tputs(sock, bf, actual_size);
-        pending_data -= actual_size;
+      size_t bytes_read = fread(bf, 1, actual_size, file);
+      if (bytes_read != actual_size) {
+        putlog(LOG_FILES, "*", "DCC file read stopped early (%s)",
+            ferror(file) ? strerror(errno) : "unexpected end of file");
+        break;
       }
+      tputs(sock, bf, bytes_read);
+      pending_data -= bytes_read;
     } while (!sock_has_data(SOCK_DATA_OUTGOING, sock) && pending_data != 0);
     free(bf);
   }
